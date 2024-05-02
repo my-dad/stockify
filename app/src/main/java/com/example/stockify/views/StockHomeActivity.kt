08c074971b530a7f9cb00ada3304ea733d1db746
cmabrur <mabrur.chowdhury@tigerit.com>
2024-05-02 13:19:04 +0600

    Search Functionality added

--- app/src/main/java/com/example/stockify/views/StockHomeActivity.kt
+++ app/src/main/java/com/example/stockify/views/StockHomeActivity.kt
@@ -1,10 +1,11 @@
 package com.example.stockify.views
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,7 +16,6 @@ import com.example.stockify.Constants.SHARED_PREF_NAME
 import com.example.stockify.PreferenceManager
 import com.example.stockify.StockListAdapter
 import com.example.stockify.database.dao.StockDetailsDao
-import com.example.stockify.database.dao.UserDao
 import com.example.stockify.databinding.ActivityStockHomeBinding
 import com.example.stockify.model.StockDetails
 import com.example.stockify.model.StockPayload
@@ -28,6 +28,11 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.util.Calendar
+import android.R.id
+import android.content.Intent
+import android.text.Editable
+import android.text.TextWatcher
+import com.example.stockify.R
 
 
 class StockHomeActivity : BaseActivity<ActivityStockHomeBinding>() {
@@ -35,6 +40,7 @@ class StockHomeActivity : BaseActivity<ActivityStockHomeBinding>() {
     private lateinit var stockDao: StockDetailsDao
     private var stockList : List<StockDetails> = listOf()
     private lateinit var adapter: StockListAdapter
+    var mMenu: Menu? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -98,13 +104,38 @@ class StockHomeActivity : BaseActivity<ActivityStockHomeBinding>() {
         binding.recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         binding.recyclerView.visibility= View.GONE
         binding.progressCircular.visibility = View.VISIBLE
-        binding.extendedFloatingActionButton.setOnClickListener {
-            val intent = Intent(this@StockHomeActivity, BoughtStockListActivity::class.java)
-            startActivity(intent)
-
+        binding.topAppBar.setOnMenuItemClickListener{
+            when (it.itemId) {
+                R.id.search -> {
+                    // Handle search action
+                    true
+                }
+                R.id.detailsPage -> {
+                    val intent = Intent(this@StockHomeActivity, BoughtStockListActivity::class.java)
+                    startActivity(intent)
+                    true
+                }R.id.logout -> {
+                    // Handle settings action
+                    true
+                }
+                // Add cases for other menu items as needed
+                else -> false
+            }
         }
 
+        binding.searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                search(s.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
     }
+
 
     fun saveStockToDb(stockDetails: StockDetails) {
         CoroutineScope(Dispatchers.IO).launch {
@@ -120,6 +151,18 @@ class StockHomeActivity : BaseActivity<ActivityStockHomeBinding>() {
 
         // Clear Room Database
         //(application as Application).database.clearAllTables()
+    }
+
+    private fun search(query: String) {
+        val filteredList = stockList.filter {
+            it.symbol.startsWith(query)
+        }
+        updateRecyclerView(filteredList)
+    }
+
+    private fun updateRecyclerView(newList: List<StockDetails>) {
+        adapter.submitList(newList)
+        adapter.notifyDataSetChanged()
     }
 
 }
